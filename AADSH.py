fa9130515bf136dd70db4483242f8944e6159d41
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-11-13 12:24:15 -0300

    dicionarios

---
+++ AADSH.py
@@ -0,0 +1,24 @@
+#dicionario para: As Aventuras De Sherlock Holmes
+def dicionario():
+    Dicionario = ()
+    SzLimit = 4
+
+    while True:
+        try:
+            Linha = input().split()
+            if Linha != "":
+                for i in range (len(Linha)):
+                    Palavra = Linha[i]
+                    if (Palavra)>=4:
+                        k = Dicionario.get(Linha[i])
+                        if k == None:
+                            Dicionario(Linha[i])=0
+                        else:
+                            Dicionario(Linha[i])+=1
+        except EOFError as Er:
+            break
+    for chave, valor in Dicionario.items():
+        print(chave, valor)
+
+if __name__=="__main__":
+ dicionario()
